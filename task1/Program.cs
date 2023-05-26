31bb74877bb25e82940132a0e8b4135e66bba40f
Maria Popova <marypoppens14@gmail.com>
2023-05-26 14:55:48 +0300

    task number 1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив, заполненный случайными положительными трёхзначными числами
+// Написать программу, которая покажет количество чётных чисел в массиве
+
+int[] GenArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+    Console.WriteLine(String.Join(",", array));
+    return array;
+}
+
+int[] array = GenArray(10);
+for (int i = 0; i < 10; i++)
+{
+    if (array[i]%2 == 0)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
